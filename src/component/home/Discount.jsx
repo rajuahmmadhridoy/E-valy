0316e0fbd4f15e3f6b2a5b843aba5ea6b36e1755
rajuahmmadhridoy <rajuahmmad0187@gmail.com>
2021-12-31 17:02:19 +0600

    dis add

--- src/component/home/Discount.jsx
+++ src/component/home/Discount.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
+import mobile from'assets/images/image-removebg-preview.png'
 const Discount = () => {
     return (
         <Container className="discount_Component">
@@ -16,19 +16,21 @@ const Discount = () => {
                             <p>Smart Phone</p>
                             <h4>Starting at <span>৳16999 </span>৳10999 </h4>
                         </div>
+                        <img src={mobile} alt="" />
                     </div>
                     </Link>
                 </Col>
                 <Col md={6}>
                     <Link to="/">
-                    <div className="discountInner mb-5">
+                    <div className=" discountInner mb-5">
                         <div className="discountContent">
-                            <button className="bg-primary">NOW</button>
+                            <button>NOW</button>
                             <p>Samsung</p>
                             <h4>Samsung Galaxy A21s Smartphone 6.5" - 4GB RAM - 64GB ROM - 48MP Camera</h4>
                             <p>Smart Phone</p>
                             <h4>Starting at <span>৳16999 </span>৳10999 </h4>
                         </div>
+                        <img src={mobile} alt="" />
                     </div>
                     </Link>
                 </Col>
